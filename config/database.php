88141d0637bc98fedcc6be327659c24c2f776db3
Maciej Babiarz <maciej.babiarz@gmail.com>
2017-10-01 19:52:56 +0200

    sql_db

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+// used to connect to the database
+$host = "";
+$db_name = "";
+$username = "";
+$password = "";
+
+
+try {
+    $conn = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+
+// show error
+
+catch (PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
